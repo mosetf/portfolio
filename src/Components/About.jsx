bcc2674df1d233180b47624ef375510fbbe62092
stan <119288936+mosetf@users.noreply.github.com>
2024-02-02 22:04:53 +0000

    Update About component description and detailOrQuote

--- src/Components/About.jsx
+++ src/Components/About.jsx
@@ -24,7 +24,7 @@ const imageAltText = "purple and blue abstract background";
  * Sort description that expands on your title on the Home component.
  */
 const description =
-  "I'm a Backend developer student studying at AlX academy. I enjoy creating unique and simple solutions to complex problems. I am passionate about creating inclusive and accessible tech for all. I am currently learning about backend development and I am excited to see where this journey takes me.";
+  "I'm a Backend developer student studying at AlX academy.I enjoy creating unique and simple solutions to complex problems. ";
 
 /**
  * List of some of skills or technologies you work on, are learning,
@@ -45,7 +45,7 @@ const skillsList = [
  * about you on a professional level.
  */
 const detailOrQuote =
-  "I am passionate about solving problems in new creative ways to drive innovation. By leveraging my UI/UX experience I continually look for new and better ways to make tech accessible by all.";
+  " I am passionate about creating inclusive and accessible tech for all. I am currently learning about backend development and I am excited to see where this journey takes me.";
 
 const About = () => {
   return (
